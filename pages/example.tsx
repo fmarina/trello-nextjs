e635f4f56e1215401b6352b220852c2471259c75
Gonchi <ghernandez@loopstudio.dev>
2022-11-22 12:23:33 -0300

    feat: add intl to example feature

--- pages/example.tsx
+++ pages/example.tsx
@@ -1,5 +1,14 @@
+import { GetStaticPropsContext } from 'next';
 import { Example as ExampleFeature } from '../features/Example';
 
 export default function Example() {
   return <ExampleFeature />;
+}
+
+export async function getStaticProps({ locale }: GetStaticPropsContext) {
+  return {
+    props: {
+      messages: (await import(`../locales/${locale}.json`)).default,
+    },
+  };
 }
